9599f8c3533e0f8eccdfbdb754603a4318cf1531
alfolilaflorida <alfolilaflorida@gmail.com>
2025-09-22 22:57:30 -0300

    Create missing package.json file

---
+++ resources/js/app.js
@@ -0,0 +1,16 @@
+import './bootstrap';
+import 'bootstrap';
+
+// SweetAlert2 global configuration
+window.Swal = require('sweetalert2');
+
+// Global CSRF token setup for AJAX requests
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
